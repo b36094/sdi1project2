d1b65293b639461e824f20b2c3929f150953a3b8
Nactus <nactusberrilli@gmail.com>
2013-01-16 15:18:57 -0800

    added a number function

--- rename-me.js
+++ rename-me.js
@@ -4,7 +4,7 @@
 //Story Title: "The adventures of mount Murrat." 
 
 //initial variables----------------------------------------------
-var groupNames = ["Andrei"," Zach"," Chrissy"," Jonathan"], 
+var groupNames = ["Andreies"," Porktornado"," Chellindae"," TheUglyStick"], 
 	storyTitle = "The Adventures of Mount Murrat.",
 	playerNum = 4;
 	
@@ -15,7 +15,7 @@ var groupNames = ["Andrei"," Zach"," Chrissy"," Jonathan"],
 //"procedure" implementation
     function firstProcedure(a) {
 	//output first line of story
-	console.log("Welcome to "+storyTitle);	
+	console.log("Welcome to the game:\""+storyTitle,"\"");	
 	console.log("There are " +playerNum, "players in the game: " +groupNames,".");
 	//checks to see if the parameter "a" is a number	
 	if (!isNaN(a)) {
@@ -27,10 +27,11 @@ var groupNames = ["Andrei"," Zach"," Chrissy"," Jonathan"],
 };
 
 
+
 // "boolean function" implementation
 var firstBosses = function (boss1,boss2){
 	if (boss1 === "Anael" && boss2 ==="Grotesk") {
-		console.log("They entered the 'Chamber of Fire', and defeated 'Lord Anael',");
+		console.log("The players entered the 'Chamber of Fire', and defeated 'Lord Anael',");
 		console.log("and his servant 'Grotesk'.");
 		return true;	
 	}
@@ -43,14 +44,35 @@ var firstBosses = function (boss1,boss2){
 
 //"number function" implementation
 var numberFunction = function (num) {
+	//local vars
+	var boss = "the Drakonian beast",
+		totalPlayers = playerNum;
+	console.log("But while they were celebrating their victory, the "+boss);
+	console.log("appeared from nowhere, furiously attacking the group.");
 	
+	while (totalPlayers > 1 ) {
 		
-		
+		console.log(+totalPlayers," player/s alive.");
+		totalPlayers--;
+				
+	}
+	console.log("Chellindae is the only player left alive.");	
+	return 	totalPlayers;
 };
 
 //---------------------------------------------------------------
+
 //calls first procedure, 1 argument
-firstProcedure(1);
+firstProcedure(2);
+
 
 //calls boolean function, 2 arguments and catches the return value into the var. "FirstBossesRt"
 var firstBossesRt = firstBosses("Anael","Grotesk");
+
+
+//calls number function, 1 argument --> playerNum and catches the number of players left alive
+var numberFunctionRt = numberFunction(playerNum);
+console.log(numberFunctionRt);
+
+//calls string function, 2 arguments --> verb and location and catches the returned string
+var stringFunctionRt = stringFunction("looks","backpack");
