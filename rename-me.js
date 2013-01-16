16f8e5ee65bf0fb16666b105a316a4b52c11d3c5
Nactus <nactusberrilli@gmail.com>
2013-01-16 10:28:31 -0800

    added initial variables

--- rename-me.js
+++ rename-me.js
@@ -1,1 +1,9 @@
-alert("JavaScript works!");
+//Andrei Birsan
+//1-16-2013
+//SDI 1
+//Story Title: "The adventures of mount Murrat." 
+
+//initial variables
+var groupNames ["Andrei","Zach","Chrissy","Jonathan"], 
+	storyTitle = "The Adventures of Mount Murrat.";,
+	playerNum = 4;
